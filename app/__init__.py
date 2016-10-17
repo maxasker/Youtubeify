bc864d937a5c07532632f3f3125fa4d7b3d58850
maxasker <maxasker@hotmail.com>
2016-10-17 14:46:22 +0200

    andra pushen

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'this is the homepage'
+    poop="poop"
+    return render_template("index.tpl")
 
 if __name__ == "__main__":
     app.run(debug=True)
